6ab9be1a10bbf39e64375aca616388b04367f988
peerakarn <peerakarn.jit@gmail.com>
2022-03-26 13:29:54 +0700

    add addwallet instruction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,23 @@ export default function Index(props: Props) {
         <title>Atadian Loan Lending</title>
         <meta name="description" content="Atadian Loan Lending"/>
       </Head>
+      <div className="max-w-xl rounded-lg overflow-hidden bg-zinc-800">
+        <div className="font-bold text-2xl mb-5">◎ Make us trust you</div>
+        <p>
+          In order to get higher tier loan packages, you need to tell us how many wallets you own. 
+          Please follow the following  step before jumping into lending submit process<br />
+          <br />
+          ◎   Connect your wallet
+          <br /><br />
+          ◎   Click add wallet
+          <br /><br />
+          ◎   Disconnect your wallet. After that, change your wallet in your browser extention then connect wallet again.
+          <br /><br />
+          ◎   Again, click add wallet
+          <br /><br />
+          Note: You should have at least 1 wallet that have been created for a while.
+        </p>
+      </div>
       <VerifyWalletView user={user}/>
     </div>
   );
